83302706e105b84225ebc2069ef60cd5e0b1b581
InnaBondarenko87 <bondarenko211287@gmail.com>
2025-04-18 23:01:39 +0200

    api

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '49743033-5279597e0b6160ec8a63a5c3f';
+
+export async function getImagesByQuery(query, page) {
+  return await axios('https://pixabay.com/api/?', {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 16,
+      page,
+    },
+  });
+}
